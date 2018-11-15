abc2639a17c0bd6708093319c63e883ac68d5bab
shai95670 <39349761+shai95670@users.noreply.github.com>
2018-11-15 17:34:48 +0200

    Add files via upload

---
+++ tetrisBlocks.js
@@ -0,0 +1,123 @@
+let tetreminoBlockI = [
+  {
+    color: 'cyan',
+    width: 32,
+    height: 32,
+    posX: 192,
+    posY: 0
+  },
+  {
+    color: 'cyan',
+    width: 32,
+    height: 32,
+    posX: 224,
+    posY: 0
+  },
+  {
+    color: 'cyan',
+    width: 32,
+    height: 32,
+    posX: 256,
+    posY: 0
+  },
+  {
+    color: 'cyan',
+    width: 32,
+    height: 32,
+    posX: 287,
+    posY: 0
+  }
+];
+
+let tetreminoBlockJ = [
+  {
+    color: 'purple',
+    width: 32,
+    height: 32,
+    posX: 192,
+    posY: 0
+  },
+  {
+    color: 'purple',
+    width: 32,
+    height: 32,
+    posX: 224,
+    posY: 0
+  },
+  {
+    color: 'purple',
+    width: 32,
+    height: 32,
+    posX: 256,
+    posY: 0
+  },
+  {
+    color: 'purple',
+    width: 32,
+    height: 32,
+    posX: 256,
+    posY: 32,
+  }
+];
+
+let tetreminoBlockL = [
+  {
+    color: 'orange',
+    width: 32,
+    height: 32,
+    posX: 192,
+    posY: 0
+  },
+  {
+    color: 'orange',
+    width: 32,
+    height: 32,
+    posX: 224,
+    posY: 0
+  },
+  {
+    color: 'orange',
+    width: 32,
+    height: 32,
+    posX: 256,
+    posY: 0
+  },
+  {
+    color: 'orange',
+    width: 32,
+    height: 32,
+    posX: 192,
+    posY: 32
+  }
+];
+
+let tetreminoBlockO = [
+  {
+    color: 'yellow',
+    width: 32,
+    height: 32,
+    posX: 192,
+    posY: 0
+  },
+  {
+    color: 'yellow',
+    width: 32,
+    height: 32,
+    posX: 224,
+    posY: 0
+  },
+  {
+    color: 'yellow',
+    width: 32,
+    height: 32,
+    posX: 192,
+    posY: 32
+  },
+  {
+    color: 'yellow',
+    width: 32,
+    height: 32,
+    posX: 224,
+    posY: 32
+  }
+];
