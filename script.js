04f9b656b70f5887bc76693475ea729dc299a714
shai isenhower <shaieisenhower1994@gmail.com>
2019-06-05 18:19:14 +0300

    updated 2 files

--- script.js
+++ script.js
@@ -1,100 +1,99 @@
-import {
-  tetreminoBlockI,
-  tetreminoBlockJ,
-  tetreminoBlockL,
-  tetreminoBlockO,
-  tetreminoBlockS,
-  tetreminoBlockT,
-  tetreminoBlockZ
-} from './tetrisBlocks.js';
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const height = canvas.height;
 const width = canvas.width;
 let dx = 2;
 let dy = 2;
-let currentBlock;
+let cells= [];
 
-// create the tetris board
-let boardBlock = {
-  color: 'black',
-  width: 32,
-  height: 32,
-  posX: 0,
-  posY: 0
-};
+// 1 - blocked cell 
+class TetrisBoard {
+  constructor(){
+    this.columns = 10;
+    this.rows = 14;
+    TetrisBoard.prototype.cellWidth = width / this.columns;
+    TetrisBoard.prototype.cellHeight = height / this.rows;
+    this.grid = [
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+  }
 
-let tetrisBoard = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-const drawBoard = () => {
-  for (var index = 0; index < tetrisBoard.length; index++) {
-    for (var j = 0; j < tetrisBoard[index].length; j++) {
-      if (tetrisBoard[index][j] === 0) {
-        ctx.fillStyle = boardBlock.color;
-        ctx.fillRect(boardBlock.posX, boardBlock.posY, boardBlock.width, boardBlock.height);
+  drawBoard(){
+    for (let row = 0; row < this.rows; row++) {
+      for (let column = 0; column < this.columns; column++) {
+      
+        let cellX = this.cellWidth * column;
+        let cellY = this.cellHeight * row;
+        //cells.push({cellX, cellY});      
+  
+        //fill the obstacles in with red
+        if (this.grid[row][column] === 1) {
+          ctx.fillStyle = 'purple';
+          //ctx.rect(cellX, cellY, this.cellWidth, this.cellHeight);
+        } 
+        else {
+          ctx.fillStyle = 'black';
+        }
+  
+        ctx.fillRect(cellX, cellY, this.cellWidth, this.cellHeight);
       }
-      boardBlock.posX += 32;
     }
-    boardBlock.posX = 0;
-    boardBlock.posY += 32;
-  }
-};
+    console.log(cells);
+    console.log(TetrisBoard.prototype.cellWidth, TetrisBoard.prototype.cellHeight);
 
-// create the Tetromino blocks
-// maybe use the boardBlock object to create the tetromino blocks
-// tetremino block will be built by multiple boardBlock objects
-let terminoBlocksArray = [
-  tetreminoBlockI,
-  tetreminoBlockJ,
-  tetreminoBlockL,
-  tetreminoBlockO,
-  tetreminoBlockS,
-  tetreminoBlockT,
-  tetreminoBlockZ
-];
-// let currentBlock = terminoBlocksArray[Math.floor(Math.random() * terminoBlocksArray.length)];
-
-const drawTerminoBlock = () => {
-  currentBlock = terminoBlocksArray[Math.floor(Math.random() * terminoBlocksArray.length)];
-  for (var i = 0; i < currentBlock.length; i++) {
-    ctx.fillStyle = currentBlock[i].color;
-    ctx.fillRect(currentBlock[i].posX, currentBlock[i].posY,
-      currentBlock[i].width, currentBlock[i].height);
-  }
-};
-
-
-
-// test method to move a block
-const moveTest = () => {
-  debugger;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawTerminoBlock();
-  for (var i = 0; i < currentBlock.length; i++) {
-    currentBlock[i].posY += dy;
-    if (currentBlock[i].posY === 480) { //colided with the bottom border
-        dy = 0;
-    }
-  }
+  };
 }
 
-drawBoard();
-setInterval(moveTest, 10);
+class Brick extends TetrisBoard {
+  constructor(playerIndexX, playerIndexY){
+    super();
+    this.playerIndexX = playerIndexX;
+    this.playerIndexY = playerIndexY;
+    this.playerPixelX = this.playerIndexX * super.cellWidth;
+    this.playerPixelY = this.playerIndexY * super.cellHeight;
+  }
+
+  drawBrick() {
+    ctx.fillStyle = 'green';
+    ctx.fillRect(this.playerPixelX, this.playerPixelY, super.cellWidth, super.cellHeight);
+  }
+};
+
+document.onkeydown = (event) => {
+  if (event.keyCode === 38 && brick.playerIndexY > 0 && !tetrisGrid.grid[brick.playerIndexY-1][brick.playerIndexX]) { //up
+    brick.playerPixelY -= 1;
+    console.log('up');
+    
+  } else if (event.keyCode === 39 && !tetrisGrid.grid[tetrisGrid.columns][brick.playerIndexX]) {
+    brick.playerPixelX += 1;
+    console.log('left');
+    
+  } else if (event.keyCode === 80) {
+    initializeGame();
+  }
+};
+
+const tetrisGrid = new TetrisBoard(width, height);
+const brick = new Brick(5, 3);
+
+const main = () => {
+  ctx.clearRect(0, 0, width, height);
+  tetrisGrid.drawBoard();
+  brick.drawBrick(2, 2);
+}
+
+
+setInterval(main, 50);
